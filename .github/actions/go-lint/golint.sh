01cdc28f2e78cc9659a9c5def8525333b0a38c6a
mokjunneng <mokjunneng@gmail.com>
2021-04-19 23:46:46 +0800

    fix: golangci-lint command not found

--- .github/actions/go-lint/golint.sh
+++ .github/actions/go-lint/golint.sh
@@ -6,13 +6,13 @@ CONFIG_PATH=$3
 
 #### Installation ####
 # binary will be $(go env GOPATH)/bin/golangci-lint
-# curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.39.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.39.0
 
 # or install it into ./bin/
 # curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.39.0
 
 # In alpine linux (as it does not come with curl by default)
-wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.39.0
+# wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.39.0
 
 #### Run ####
 golangci-lint run ${COMMA_DELIMITED_PATHS//,/} $( if [ -n "$CONFIG_PATH" ]; then echo "--config $CONFIG_PATH"; fi ) --out-format checkstyle > $OUTPUT_PATH
