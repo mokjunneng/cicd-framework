926ea92d808c61c0580b58f52084f3f4f0995c62
mokjunneng <mokjunneng@gmail.com>
2021-05-03 17:27:39 +0800

    fix: gorelease action

--- .github/actions/goreleaser/gorelease.sh
+++ .github/actions/goreleaser/gorelease.sh
@@ -3,9 +3,6 @@
 # Install goreleaser
 curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | sh
 
-ARGS = $1
-WORKDIR = $2
+ARGS=$1
 
-cd ${WORKDIR}
-
-gorelease ${ARGS}
+./bin/goreleaser ${ARGS}
